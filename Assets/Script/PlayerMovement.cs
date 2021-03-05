478e2ceb72a267fad615b3574cb2204fdc69de44
HongCFull <zhujiangken@gmail.com>
2021-03-05 15:06:52 +0800

    Updated player control + camera

--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -4,14 +4,19 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] float playerSpeed = 2.0f;
-    [SerializeField] float jumpHeight = 1.0f;
-    [SerializeField] float gravityValue = -9.81f;
-    
-    private CharacterController controller;
-    private Vector3 playerVelocity;
-    private bool groundedPlayer;
+    [SerializeField] float speed = 20f;   
+    [SerializeField] float gravity =-9.81f;
+    [SerializeField] float jumpHeight = 10;
 
+    [SerializeField] private LayerMask groundMask;
+
+    private CharacterController controller;
+
+    
+    private Vector3 jumpVelocity ;
+    private Vector3 moveDirection;
+    private bool isGrounded = true;
+    private float distanceToGround;
     void Start()
     {
         controller = GetComponent<CharacterController>();
@@ -19,25 +24,37 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
-        groundedPlayer = controller.isGrounded;
-        if (groundedPlayer && playerVelocity.y < 0)
-            playerVelocity.y = 0f;
-            
-        Vector3 move = new Vector3(Input.GetAxis("Horizontal"),0, Input.GetAxis("Vertical"));
-        controller.Move(move*Time.deltaTime*playerSpeed);
+        Move();
+    }
 
-        
-        if (Input.GetButtonDown("Jump") && groundedPlayer) {
-            playerVelocity.y += Mathf.Sqrt(jumpHeight * -2.0f * gravityValue);
+    private float dampAngle;    //for damping angle function
+    void Move(){
+        distanceToGround= controller.height/2 +0.1f;
+        isGrounded=Physics.CheckSphere(transform.position,distanceToGround,groundMask);
+
+        if(isGrounded && jumpVelocity.y <0){  
+            jumpVelocity.y = -2f;
         }
-        
-        playerVelocity.y += gravityValue*Time.deltaTime ;
-        controller.Move(playerVelocity *Time.deltaTime);
-    
-      
+
+        if(isGrounded && Input.GetKeyDown(KeyCode.Space)){
+            jumpVelocity.y = Mathf.Sqrt(jumpHeight*-2*gravity);
+        }
+
+        float z = Input.GetAxis("Vertical");
+        float x = Input.GetAxis("Horizontal");
+        moveDirection= new Vector3(x,0,z);
+        moveDirection*=speed;
+        controller.Move(moveDirection*Time.deltaTime);
+
+//change player's head
+        float smoothTime =0.1f;
+        if(moveDirection.magnitude>=0.1f){
+            float targetAngle = Mathf.Atan2(moveDirection.x,moveDirection.z) *Mathf.Rad2Deg;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y,targetAngle,ref dampAngle,smoothTime);
+            transform.rotation = Quaternion.Euler(0,angle,0);
+        }
+
+        jumpVelocity.y+=gravity*Time.deltaTime;
+        controller.Move(jumpVelocity*Time.deltaTime);
     }
 }
-
-/*
-
-    */
