27def413536885d94e8caa7cfa59a0fc305ea878
HongCFull <zhujiangken@gmail.com>
2021-03-13 14:29:18 +0800

    twisted camera attribute

--- Assets/Script/Player/CameraController.cs
+++ Assets/Script/Player/CameraController.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    [SerializeField][Range(0,-90)] float minAngle;
-    [SerializeField][Range(0,90)]  float maxAngle;
+    [SerializeField][Range(0,-90)] float minAngle =-45;
+    [SerializeField][Range(0,90)]  float maxAngle=45;
 
     public float RotationSpeed = 1;
     public Transform Target, Player;
@@ -16,7 +16,6 @@ public class CameraController : MonoBehaviour
     {
         Cursor.visible=false;
         Cursor.lockState= CursorLockMode.Locked;    //lock mouse into the center of the screen
-
 
     }
 
@@ -32,7 +31,6 @@ public class CameraController : MonoBehaviour
         mouseY = Mathf.Clamp(mouseY,minAngle,maxAngle);         //restrict the mouseY (up/down dependent) to specified range 
 
         transform.LookAt(Target);
-        //Target.transform.position.x +=2.5;
         Target.rotation= Quaternion.Euler(-mouseY,mouseX,0);    //allow the camera "look upward"
         Player.rotation = Quaternion.Euler(0,mouseX,0); //only allow the player to turns their 'head' on x-z plane
 
