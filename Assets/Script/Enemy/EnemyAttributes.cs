d3e99446437b7152871b5869fd32834b4b3e22bd
Glass Hammer <78712769+Glass-Hammer@users.noreply.github.com>
2021-03-16 02:29:57 +0800

    untracked files

---
+++ Assets/Script/Enemy/EnemyAttributes.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAttributes : MonoBehaviour
+{
+    public BattleAttributes enemyBA;
+    public float enemySpeed;
+    public float alertArea;
+    public float battleTriggerArea;
+    public float attackArea;
+}
