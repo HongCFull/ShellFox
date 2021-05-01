637c1945b9fe97040fade2d6320aa5406f12ba8e
HongCFull <zhujiangken@gmail.com>
2021-05-01 17:49:11 +0800

    stage missing file

--- Assets/Script/Global Battle/AttackSkills/WaterJetAttack.cs
+++ Assets/Script/Global Battle/AttackSkills/WaterJetAttack.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class WaterJetAttack : BattleSkill
 {
-    [SerializeField] float Damage ;
+    [SerializeField] float Power ;
     [SerializeField] float EnergyCost ;
     [SerializeField] float CoolDown;
     [SerializeField] float CastTime;
@@ -10,7 +10,7 @@ public class WaterJetAttack : BattleSkill
 
     private void Awake() {
         skillElement=BattleType.Element.WATER;
-        power=Damage;
+        power=Power;
         energyCost=EnergyCost;
         coolDown=CoolDown;
         castTime=CastTime; 
